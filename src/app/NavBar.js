c436f2d5bc9fd23587a915a05de558731c1e16b6
Vin R. <vramroop6@gmail.com>
2023-10-28 22:52:16 -0400

    die

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -4,14 +4,14 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <div className="bg-blue-400 p-4 flex justify-between items-center">
-      <div className="flex items-center space-x-4">
-        <Link href="/">
+      <div className="flex items-center text-center space-x-4">
+        <Link legacyBehavior href="/">
           <a className="text-white hover:bg-blue-500 hover:text-black rounded-md px-2 py-1">Home</a>
         </Link>
-        <Link href="/planner">
+        <Link legacyBehavior href="/planner">
           <a className="text-white hover:bg-blue-500 hover:text-black rounded-md px-2 py-1">Planner</a>
         </Link>
-        <Link href="/resources">
+        <Link legacyBehavior href="/resources">
           <a className="text-white hover:bg-blue-500 hover:text-black rounded-md px-2 py-1">Resources</a>
         </Link>
       </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,8 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import TextWidget from './TextWidget';
-import Clock from 'react-live-clock'; 
-import Planner from './Planner';
+import Clock from 'react-live-clock';
+import Navbar from './navbar';
 
 const Home = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -13,38 +12,29 @@ const Home = () => {
     document.documentElement.classList.toggle('dark');
   };
 
-  const [isClient, setIsClient] = useState(false)
- 
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
-    setIsClient(true)
-  }, [])
-  
+    setIsClient(true);
+  }, []);
 
   return (
     <div className={`flex flex-col min-h-screen ${isDarkMode ? 'dark bg-white' : 'bg-blue-400'}`}>
-      <Clock 
-          format={'h:mm:ssa'} 
-          style={{fontSize: '1.5em'}} 
-          ticking={true} className='text-white text-center my-2'/> 
+      <Navbar className="text-center items-center"/>
+      <Clock format={'h:mm:ssa'} style={{ fontSize: '1.5em' }} ticking={true} className='text-white text-center my-2' />
       <div className="flex-grow flex flex-col items-center justify-center">
-        <h1 className={`text-4xl mb-4 font-bold  align-top ${isDarkMode ? 'text-black' : 'text-white'}`}>Welcome to Studently</h1>
+        <h1 className={`text-4xl mb-4 font-bold  align-top ${isDarkMode ? 'text-black' : 'text-white'}`}>
+          Welcome to Studently
+        </h1>
         <div className='flex gap-4'>
           <div className={`border border-blue-300 rounded-lg p-4 shadow-md ${isDarkMode ? 'text-black' : 'text-white'}`}>
             <p>Made by Students, for Students</p>
             <img src="https://github.com/vintheruler1/Studiously/blob/main/src/app/studiously.png?raw=true" alt="logo" />
           </div>
         </div>
-          <TextWidget/>
-          <Planner className="container mx-auto mt-8"/>
-          {/* <Player/> */}
-          
+        {/* <TextWidget />
+        <Planner className="container mx-auto mt-8" /> */}
       </div>
-      {/* <button
-        className={`bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded m-4 ${isDarkMode ? 'dark:bg-gray-800' : ''}`}
-        onClick={toggleTheme}
-      >
-        Toggle {isDarkMode ? 'Light' : 'Dark'} Mode
-      </button> */}
       <footer className={`text-sm align-middle text-center text-gray-500 ${isDarkMode ? 'dark:text-gray-400' : ''}`}>
         &copy; 2023 Studently Inc. All Rights Reserved.
       </footer>
