7cb6b7d8a579914fef3cf69bc9dadf6a566469e2
Vintheruler1 <83379181+vintheruler1@users.noreply.github.com>
2023-10-30 06:53:50 -0400

    Update NavBar.js

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -7,18 +7,18 @@ const Navbar = () => {
       <div className="flex justify-between items-center">
         <div>
           <Link legacyBehavior href="/">
-            <a className="text-white text-lg mt-2 text-center rounded-full px-6 py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Studiously</a>
+            <a className="text-white text-lg mt-2 text-center rounded-full px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Studiously</a>
           </Link>
         </div>
         <div className="flex items-center space-x-4">
           <Link legacyBehavior href="/Planner">
-            <a className="text-white text-lg mt-2 text-center rounded-full px-6 py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Planner</a>
+            <a className="text-white text-lg mt-2 text-center rounded-full px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Planner</a>
           </Link>
           <Link legacyBehavior href="/Resources">
-            <a className="text-white text-lg mt-2 text-center rounded-full px-6 py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Resources</a>
+            <a className="text-white text-lg mt-2 text-center rounded-full px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Resources</a>
           </Link>
           <Link legacyBehavior href="/plugins">
-            <a className="text-white text-lg mt-2 text-center rounded-full px-6 py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Tools</a>
+            <a className="text-white text-lg mt-2 text-center rounded-full px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Tools</a>
           </Link>
         </div>
       </div>
