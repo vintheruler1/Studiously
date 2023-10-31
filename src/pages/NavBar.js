2e5f53e50af05397af810f4551a95f0fe387200e
Vin R. <vramroop6@gmail.com>
2023-10-30 23:43:37 -0400

    nvm mobile users idc abt

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -3,20 +3,22 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-600 p-2 sm:p-4">
-      <div className="flex flex-col sm:flex-row items-center justify-between">
-        <Link legacyBehavior href="/">
-          <a className="text-white text-lg text-center rounded-full p-2 sm:p-4 bg-blue-500 hover:bg-blue-600 hover:text-black mb-2 sm:mb-0 sm:mr-4">Studiously</a>
-        </Link>
+    <div className="to-blue-600 p-4">
+      <div className="flex justify-between items-center">
+        <div>
+          <Link legacyBehavior href="/">
+            <a className="text-white text-lg mt-2 text-center rounded-full px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Studiously</a>
+          </Link>
+        </div>
         <div className="flex items-center space-x-4">
           <Link legacyBehavior href="/Planner">
-            <a className="text-white text-lg text-center rounded-full p-2 sm:p-4 bg-blue-500 hover:bg-blue-600 hover:text-black">Planner</a>
+            <a className="text-white text-lg mt-2 text-center rounded-full px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Planner</a>
           </Link>
           <Link legacyBehavior href="/Resources">
-            <a className="text-white text-lg text-center rounded-full p-2 sm:p-4 bg-blue-500 hover:bg-blue-600 hover:text-black">Resources</a>
+            <a className="text-white text-lg mt-2 text-center rounded-full px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Resources</a>
           </Link>
           <Link legacyBehavior href="/plugins">
-            <a className="text-white text-lg text-center rounded-full p-2 sm:p-4 bg-blue-500 hover:bg-blue-600 hover:text-black">Tools</a>
+            <a className="text-white text-lg mt-2 text-center rounded-full px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-600 hover:text-black">Tools</a>
           </Link>
         </div>
       </div>
