a89529dcb931fb3b24057046d9831c5e2ce19e10
Vintheruler1 <83379181+vintheruler1@users.noreply.github.com>
2023-10-31 09:45:21 -0400

    60th commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  // Entry point for your application
   entry: './src/index.js',
   output: {
-    // Output directory and filename for the bundled code
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
