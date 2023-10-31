f618661dee9c01fcd2af8e5ad66848f46513fb27
Vin R. <vramroop6@gmail.com>
2023-10-30 23:40:40 -0400

    Please mobile users dont bug me

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -20,15 +20,15 @@ const Home = () => {
     <div className={`min-h-screen ${isDarkMode ? 'dark bg-white' : 'bg-gradient-to-b from-blue-400 to-blue-600'}`}>
       <title>Resources | Studiously</title>
       <Navbar />
-      <div class="shape-blob"></div>
-	<div class="shape-blob one"></div>
-	<div class="shape-blob two"></div>
+      <div className="shape-blob"></div>
+      <div className="shape-blob one"></div>
+      <div className="shape-blob two"></div>
       <div className="flex flex-col items-center justify-center h-full">
         <div className="text-center my-2">
           <Clock format={'h:mm:ssa'} style={{ fontSize: '1.5em' }} ticking={true} className='text-white' />
         </div>
         <div className="w-full sm:w-11/12 lg:w-4/5 xl:w-3/5">
-          <h1 className={`text-5xl mb-4 font-bold text-center my-6 py-6 ${isDarkMode ? 'text-white' : 'text-white'}`}>
+          <h1 className={`text-5xl mb-4 font-bold text-center my-6 py-6 ${isDarkMode ? 'text-white' : 'text-black'}`}>
             Resources
           </h1>
           <div className='flex flex-wrap gap-4 justify-center'>
@@ -55,10 +55,10 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <footer className={`text-sm text-center text-white`}>
+      <footer className={`text-sm text-center ${isDarkMode ? 'text-white' : 'text-black'}`}>
         &copy; 2023 Studiously Inc. All Rights Reserved |{' '}
-        <a href="/AboutUs" className="text-white hover:underline">About Us</a> |{' '}
-        <a href="/terms-and-conditions" className="text-white hover:underline">Terms and Conditions</a>
+        <a href="/AboutUs" className={isDarkMode ? 'text-white hover:underline' : 'text-black hover:underline'}>About Us</a> |{' '}
+        <a href="/terms-and-conditions" className={isDarkMode ? 'text-white hover:underline' : 'text-black hover:underline'}>Terms and Conditions</a>
       </footer>
     </div>
   );
@@ -67,13 +67,11 @@ const Home = () => {
 const ResourceCard = ({ title, description, isDarkMode }) => {
   return (
     <div className={`w-full sm:w-1/2 lg:w-1/2 xl:w-1/3 p-4`}>
-      <div className={`border-2 border-white rounded-lg p-4 shadow-md ${isDarkMode ? 'text-white' : 'text-white'}`}>
+      <div className={`border-2 border-white rounded-lg p-4 shadow-md ${isDarkMode ? 'text-white' : 'text-black'}`}>
         <p className='text-2xl text-center font-bold py-2 my-2'>
           {title}
         </p>
-        <p className='text-white text-lg py-2 my-2'>
-          {description}
-        </p>
+        <p className={isDarkMode ? 'text-white' : 'text-black'}>{description}</p>
       </div>
     </div>
   );
